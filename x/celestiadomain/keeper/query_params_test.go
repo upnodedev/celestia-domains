7af405a4ffd6cb12fc8d0492f72aa96e315e5c6e
Developer Experience team at Ignite <hello@ignite.com>
2023-05-14 17:13:33 +0700

    Initialized with Ignite CLI

---
+++ x/celestiadomain/keeper/query_params_test.go
@@ -0,0 +1,21 @@
+package keeper_test
+
+import (
+	"testing"
+
+	testkeeper "celestia-domain/testutil/keeper"
+	"celestia-domain/x/celestiadomain/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParamsQuery(t *testing.T) {
+	keeper, ctx := testkeeper.CelestiadomainKeeper(t)
+	wctx := sdk.WrapSDKContext(ctx)
+	params := types.DefaultParams()
+	keeper.SetParams(ctx, params)
+
+	response, err := keeper.Params(wctx, &types.QueryParamsRequest{})
+	require.NoError(t, err)
+	require.Equal(t, &types.QueryParamsResponse{Params: params}, response)
+}
