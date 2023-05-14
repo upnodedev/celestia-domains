6448c6a6dae59c7efc8362b01e1ce6dba27e806d
Chomtana <Chomtana001@gmail.com>
2023-05-14 18:05:28 +0700

    Add domain query

---
+++ x/celestiadomain/client/cli/query_domain.go
@@ -0,0 +1,42 @@
+package cli
+
+import (
+	"strconv"
+
+	"celestia-domain/x/celestiadomain/types"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/spf13/cobra"
+)
+
+var _ = strconv.Itoa(0)
+
+func CmdDomain() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "domain",
+		Short: "Query domain",
+		Args:  cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			params := &types.QueryDomainRequest{}
+
+			res, err := queryClient.Domain(cmd.Context(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
