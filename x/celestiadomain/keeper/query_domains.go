f356a8c1f241d350ae03cac1a8cc379be78f1d58
Chomtana <Chomtana001@gmail.com>
2023-05-15 00:53:17 +0700

    Query domains and primary domain

--- x/celestiadomain/keeper/query_domains.go
+++ x/celestiadomain/keeper/query_domains.go
@@ -2,9 +2,13 @@ package keeper
 
 import (
 	"context"
+	"encoding/json"
 
 	"celestia-domain/x/celestiadomain/types"
+
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -16,8 +20,38 @@ func (k Keeper) Domains(goCtx context.Context, req *types.QueryDomainsRequest) (
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Process the query
-	_ = ctx
+	// Get store
+	store := ctx.KVStore(k.storeKey)
+	domainStore := prefix.NewStore(store, []byte("owned-domains-"+req.Owner))
 
-	return &types.QueryDomainsResponse{}, nil
+	// Paginate the recipes store based on PageRequest
+	var domains []*types.Domain
+
+	pageRes, err := query.Paginate(domainStore, req.Pagination, func(key []byte, value []byte) error {
+		var domain DomainType
+		if err := json.Unmarshal(value, &domain); err != nil {
+			return err
+		}
+
+		domainConverted := types.Domain{
+			Owner:      domain.Owner,
+			Domain:     domain.Domain,
+			Parent:     domain.Parent,
+			Expiration: domain.Expiration,
+		}
+
+		domains = append(domains, &domainConverted)
+
+		return nil
+	})
+
+	// Throw an error if pagination failed
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryDomainsResponse{
+		Domains:    domains,
+		Pagination: pageRes,
+	}, nil
 }
